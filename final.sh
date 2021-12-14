9320ec8f02be6ff071c766b5828c17c9c2afd931
ZakhariiYevchyn <95706656+ZakhariiYevchyn@users.noreply.github.com>
2021-12-13 21:45:07 -0800

    Update final.sh

--- final.sh
+++ final.sh
@@ -3,7 +3,7 @@
 if [ -z $3 ]
 then
         echo "Usage: script [file extension] [target directory] [backup directory] 
-        Example: script .text /home/arnold /backup 
+        Example: script .text /home/user /backup 
         this is backs up files from a certain directory to anohter"
 
 exit 0;
